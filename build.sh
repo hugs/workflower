f797c2eccceb45d92999adf49254155ec79946f3
Jason Huggins <jason@jrandolph.com>
2014-07-10 21:32:54 -0500

    Remove unneeded files from the app.

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ cp src/Info.plist bin/Workflower.app/Contents/Info.plist
 cp src/img/workflower.icns bin/Workflower.app/Contents/Resources/nw.icns
 cp -R src bin/Workflower.app/Contents/Resources/app.nw
 find bin/Workflower.app/Contents/Resources/app.nw -name .DS_Store | xargs rm
-
+find bin/Workflower.app/Contents/Resources/app.nw -not -name "*.node" -not -name "*.json" -type f -print0 | xargs -0 grep -l "hugs" | xargs rm
+find bin/Workflower.app/Contents/Resources/app.nw -name ".git*" -print0 | xargs -0 rm -rf
 ./bin/Workflower.app/Contents/MacOS/node-webkit 
