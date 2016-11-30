2e4b208715f795c45d61fcf4c896c208c124c527
Jason Huggins <jason@jrandolph.com>
2016-11-29 22:26:42 -0600

    Add cloud and mobile phone UI options.

--- src/js/ui.js
+++ src/js/ui.js
@@ -2,12 +2,12 @@ $(function () {
 
   $(window).bind('click', function(event) {
         console.log('Click!');
-	});        
-        
+	});
+
 	$(window).bind('keydown', function(event) {
   		if(event.keyCode === 27) {
         // Escape key
-  		}            
+  		}
       if (event.metaKey && event.keyCode==79) {
           // Command + O
           console.log ("Command + O");
@@ -25,12 +25,21 @@ $(function () {
           console.log ("Command + W");
           win.close();
           event.preventDefault()
-      }         
+      }
 	});
 
   $('#dev-tools').click(function(e) {
       win.showDevTools();
       e.preventDefault();// prevent the default anchor functionality
+  });
+
+
+  $('#cloud').click(function() {
+    $(this).toggleClass('status-enabled');
+  });
+
+  $('#mobile').click(function() {
+    $(this).toggleClass('status-enabled');
   });
 
 });
